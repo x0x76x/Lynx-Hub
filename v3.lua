6b93f236472ce62d313f11d00ecfe931f7251305
Jack <113581836+velayius@users.noreply.github.com>
2023-03-23 13:16:35 +0000

    Update v3.lua

--- v3.lua
+++ v3.lua
@@ -6,4 +6,6 @@ elseif game.PlaceId == 11998813375 then -- Cali Streets
     loadstring(game:HttpGet('https://scripts.luawl.com/hosted/4796/18014/CaliStreets.lua'))()
 elseif game.PlaceId == 4836735146 then -- Street Shootout
     loadstring(game:HttpGet('https://scripts.luawl.com/hosted/4796/18098/StreetShootout.lua'))()
+elseif game.PlaceId == 9210611498 then -- Miami Florida
+    loadstring(game:HttpGet('https://scripts.luawl.com/hosted/4796/18099/MiamiFlorida.lua'))()
 end
